e0c9e84c42b0411b4bbf4eb261a9abd667562827
zzz sss <onlineaccounts998@gmail.com>
2022-06-05 15:35:09 -0400

    manually move css

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 
 const packageJson = require("./package.json");
 
@@ -22,10 +24,14 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss({}),
+      postcss({
+        extract: true,
+      }),
+      terser(),
     ],
   },
   {
